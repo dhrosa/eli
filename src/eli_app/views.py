1d3028eb5076c763d21080c003bd7b4798a8f7ee
Diony Rosa <dhrosa@gmail.com>
2024-08-15 20:57:02 -0400

    Send query to Gemini

--- src/eli_app/views.py
+++ src/eli_app/views.py
@@ -5,6 +5,17 @@ from django.views.generic.edit import FormView
 
 from .forms import QueryForm
 
+from google import generativeai
+from os import environ
+
+generativeai.configure(api_key=environ["API_KEY"])
+ai_model = generativeai.GenerativeModel('gemini-1.5-flash')
+
+preamble = """
+Answer the following question as if you were explaining this concept to a ten-year old.
+Limit your response to 5 sentences. Don't use excessive exclamation marks.
+
+"""
 
 def submit(request: HttpRequest) -> HttpResponse:
     return HttpResponse(str(request.POST))
@@ -19,7 +30,9 @@ class SubmitView(TemplateView):
     template_name = "eli_app/submit.html"
 
     def post(self, request, *args, **kwargs):
-        print(request.POST)
+        query = preamble + request.POST["query"]
+        ai_response = ai_model.generate_content(query)
+        print(ai_response.text)
         return HttpResponseRedirect(reverse("index"))
 
     def get_context_data(self, **kwargs):
