dd9f8cad192a6ad3b89726a8aa0212eb33f7184d
Diony Rosa <dhrosa@gmail.com>
2024-08-17 11:46:41 -0400

    Remove app/ prefix from URLs.

--- src/eli/urls.py
+++ src/eli/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("app/", include("eli_app.urls")),
+    path("", include("eli_app.urls")),
 ]
