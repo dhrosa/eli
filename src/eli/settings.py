0eee5a0e5ac74d9c962d49b6a975d1235b8b5b04
Diony Rosa <dhrosa@gmail.com>
2024-08-18 14:37:53 -0400

    Allow DDNS host name

--- src/eli/settings.py
+++ src/eli/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY", "django-insecure-bs7ge(^&fo9@+8o1&x48)_gxisa%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS: list[str] = ["0.0.0.0", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS: list[str] = ["0.0.0.0", "127.0.0.1", "localhost", "eli-app.ddns.net"]
 
 GEMINI_API_KEY = env.str("GEMINI_API_KEY")
 
