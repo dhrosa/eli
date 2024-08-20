94583b4e83a566b158fda13e0b44000654a066dd
Diony Rosa <dhrosa@gmail.com>
2024-08-20 17:57:13 -0400

    Serve static files from Django when using runserver

--- src/eli/settings.py
+++ src/eli/settings.py
@@ -142,3 +142,7 @@ STATIC_ROOT = "/static"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Bypass static serving from nginx
+if env.bool("USE_RUNSERVER", False):
+    STATIC_URL = "static-dev/"
