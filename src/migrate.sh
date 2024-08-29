0986f5afceb49e77a7326d804a3a34a24f4ab864
Diony Rosa <dhrosa@gmail.com>
2024-08-29 00:36:18 -0400

    Fix wrong user in DB health check

--- src/migrate.sh
+++ src/migrate.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 echo "Waiting for database to be ready..."
-timeout 90s bash -c "until pg_isready --host db; do sleep 0.1; done"
+timeout 90s bash -c "until pg_isready --host db --user postgres; do sleep 0.1; done"
 echo "Database ready."
 exec ./manage.py migrate
