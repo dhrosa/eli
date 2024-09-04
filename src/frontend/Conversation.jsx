4ce95883a20d9f5c92ff49e5aa5c2f66a6a9ae25
Diony Rosa <dhrosa@gmail.com>
2024-09-04 14:09:43 -0400

    Start using Bulma card for conversations

--- src/frontend/Conversation.jsx
+++ src/frontend/Conversation.jsx
@@ -12,23 +12,23 @@ function Quote({ text, author }) {
 }
 
 export default function ({ data }) {
-  if (!data) {
-    return <article className="conversation" aria-busy="true"></article>;
-  }
   return (
-    <article className="conversation">
-      <header>
-        <Link to={data.url} className="material-icons">
-          link
-        </Link>
-        &nbsp;
-        {data.response_title}
-      </header>
-      <Quote text={data.query} author={data.audience_name} />
-      <Quote
-        text={data.response_text}
-        author={`ELI (${data.ai_model_name}})`}
-      />
-    </article>
+      <div className="card">
+          <div className="card-header">
+              <p class="card-header-title">
+                  {data.response_title}
+                  <Link to={data.url} className="card-header-icon icon">
+                      <span className="material-icons">link</span>
+                  </Link>
+              </p>              
+          </div>
+          <div className="card-content">
+              <Quote text={data.query} author={data.audience_name} />
+              <Quote
+                  text={data.response_text}
+                  author={`ELI (${data.ai_model_name}})`}
+              />
+          </div>
+      </div>
   );
 }
