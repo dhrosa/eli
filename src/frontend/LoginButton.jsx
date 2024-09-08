3ac5e7df432f603eb2bf3e88da4561edd17fea61
Diony Rosa <dhrosa@gmail.com>
2024-09-07 20:33:21 -0400

    Render login form errors

--- src/frontend/LoginButton.jsx
+++ src/frontend/LoginButton.jsx
@@ -1,37 +1,89 @@
 import { useState } from "react";
+import Api from "./Api";
 
 import Cookie from "js-cookie";
+
+async function parseResponse(response) {
+  const value = await response.json();
+  if (response.ok) {
+    return {
+      success: value,
+    };
+  }
+  return {
+    errors: value,
+  };
+}
 
 function UserName() {
   return Cookie.get("username") ?? "Anonymous";
 }
 
-function Form() {
+function ErrorList({ items }) {
+  if (!items) {
+    return false;
+  }
   return (
-    <form className="form">
+    <ul className="has-text-danger">
+      {items.map((e) => (
+        <li className="help">{e}</li>
+      ))}
+    </ul>
+  );
+}
+
+function Form() {
+  const [errors, setErrors] = useState(null);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const headers = new Headers();
+    const formData = new FormData(event.currentTarget);
+    const request = {
+      method: "POST",
+      headers: headers,
+      body: new FormData(event.currentTarget),
+    };
+    const response = await Api("/token/", request);
+    const data = await parseResponse(response);
+    setErrors(data.errors);
+  };
+
+  return (
+    <form className="form" onSubmit={onSubmit}>
       <div className="field">
         <label className="label">Username</label>
         <div className="control">
-          <input className="input" type="text" placeholder="User Name" />
+          <input
+            className="input"
+            type="text"
+            name="username"
+            placeholder="Username"
+          />
         </div>
+        <ErrorList items={errors?.username} />
       </div>
       <div className="field">
         <label className="label">Password</label>
         <div className="control">
-          <input className="input" type="password" />
+          <input className="input" name="password" type="password" />
         </div>
+        <ErrorList items={errors?.password} />
       </div>
+      <div className="control">
+        <button className="button is-primary" type="submit">
+          Log In
+        </button>
+      </div>
+      <ErrorList items={errors?.non_field_errors} />
     </form>
   );
 }
 
 function LoginPanel() {
   return (
-    <div className="panel">
-      <div className="panel-heading">Log In</div>
-      <div className="panel-block">
-        <Form />
-      </div>
+    <div className="box">
+      <Form />
     </div>
   );
 }
