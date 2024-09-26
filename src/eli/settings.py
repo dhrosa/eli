6724394f422d387ad98479ab71ae1e6210b1a2e4
Diony Rosa <dhrosa@gmail.com>
2024-09-25 20:31:29 -0400

    Add unhelpful-eli domain

--- src/eli/settings.py
+++ src/eli/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env.str("SECRET_KEY", "django-insecure-bs7ge(^&fo9@+8o1&x48)_gxisa%
 DEBUG = env.bool("DEBUG", False)
 
 ALLOWED_HOSTS: list[str] = ["*"]
-CSRF_TRUSTED_ORIGINS = ["https://localhost", "https://eli-app.ddns.net"]
+CSRF_TRUSTED_ORIGINS = ["https://localhost", "https://eli-app.ddns.net", "https://unhelpful-eli.ddns.net"]
 
 GEMINI_API_KEY = env.str("GEMINI_API_KEY")
 OPENAI_API_KEY = env.str("OPENAI_API_KEY")
